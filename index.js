d9a0ea4266afb515c33c301c466821f3a98c9430
Mercy Spectures <71334636+MercySpectures@users.noreply.github.com>
2024-06-11 01:10:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  password: "1813",
+  database: "LearnPostgreSQL",
+  post: 5432,
+});
+
+db.connect();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisited(){
+  const result = await db.query("SELECT country_code FROM visited_country");
+  let countries = [];
+  result.rows.forEach(row => {
+    countries.push(row.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const countries = await checkVisited();
+  res.render("index.ejs", {countries : countries, total : countries.length});
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  try{
+    const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 ||'%';", [input.toLowerCase()]);
+    const data = result.rows[0];
+    const Code = data.country_code;
+    try{
+      await db.query("INSERT INTO visited_country (country_code) VALUES ($1)", [Code]);
+      res.redirect("/");
+    } catch(err){
+      console.log(err);
+      const countries = await checkVisited();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch(err){
+    console.log(err);
+    const countries = await checkVisited();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
